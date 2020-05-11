b8720c427abbab38f445aa06031b7ba993c7287a
Cloc Badge Action <push@no-reply.github.com>
2020-05-11 13:51:27 +0800

    Auto Cloc Commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,19 @@ set
 
 echo "🎱Start to count lines of code..."
 max=`cloc .|grep -A 2 code|sed -n '3p'`
-lang=`echo $max|awk '{print $1}'`
-line=`echo $max|awk '{print $5}'`
+if [ `echo $max|awk '{print NF}'` -eq 5 ]; then
+    lang=`echo $max|awk '{print $1}'`
+else
+    lang=`echo $max|awk '{print $1$2}'`
+fi
+
+line=`echo $max|awk '{print $NF}'`
 echo $lang,$line
+
 wget https://img.shields.io/badge/$lang-$line-green?style=for-the-badge -O cloc.svg
 
 git config --global user.email "push@no-reply.github.com"
-git config --global user.name "GitHub Push Action"
+git config --global user.name "Cloc Badge Action"
 if [ -z "$(git status --porcelain)" ]; then
     echo "Clean!"
 else
