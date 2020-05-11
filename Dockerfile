aacac45a4514b9bbb69bc09bba33fbc1e609d748
GitHub Push Action <push@no-reply.github.com>
2020-05-11 13:39:30 +0800

    Auto Cloc Commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# Container image that runs your code
+FROM ubuntu:18.04
+
+# Copies your code file from your action repository to the filesystem path `/` of the container
+COPY entrypoint.sh /entrypoint.sh
+
+RUN apt-get update && apt-get install -y \
+    cloc \
+    wget
+
+# Code file to execute when the docker container starts up (`entrypoint.sh`)
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -l
+
+set
+
+echo "🎱Start to count lines of code..."
+max=`cloc .|grep -A 2 code|sed -n '3p'`
+lang=`echo $max|awk '{print $1}'`
+line=`echo $max|awk '{print $5}'`
+echo $lang,$line
+wget https://img.shields.io/badge/$lang-$line-green?style=for-the-badge&logo=appveyor -O cloc.svg
+
+git config --global user.email "push@no-reply.github.com"
+git config --global user.name "GitHub Push Action"
+if [ -z "$(git status --porcelain)" ]; then
+    echo "Clean!"
+else
+    git add --all
+    git commit -m "Auto Cloc Commit"
+    git push origin $BRANCH
+fi
